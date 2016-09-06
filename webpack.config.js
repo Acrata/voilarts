35ad10571276fd3ec406adc35bdc0fdebee85042
Acrata <hxtr227@gmail.com>
2016-09-06 02:31:52 -0430

    awbpck

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,22 @@
   var path = require('path');
     module.exports = {
-        entry: './src/js/app2.js',
+        entry: './js/app2.js',
         output: {
-            path: __dirname,
+            path: './dist/js',
             filename: 'bundle.js'
         },
         module: {
             loaders: [
                 { test: path.join(__dirname, 'es6'),
-                  loader: 'babel-loader' }
+                  loader: 'babel-loader?presets[]=es2015' },
+
+                { test: /\.scss$/,
+                    loader: ['style','css','postcss','sass'] }
             ]
+        },
+        postcss: function(){
+            return [lost,autoprefixer({
+                browsers:['last 3 versions']
+            })];
         }
     };
