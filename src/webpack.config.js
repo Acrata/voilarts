91fd1d8ad77d86112e473524d557e03f49130d9e
Acrata <hxtr227@gmail.com>
2016-09-06 02:30:47 -0430

    wbpck branch

---
+++ src/webpack.config.js
@@ -0,0 +1,22 @@
+  var path = require('path');
+    module.exports = {
+        entry: './src/js/app2.js',
+        output: {
+            path: './dist/js',
+            filename: 'bundle.js'
+        },
+        module: {
+            loaders: [
+                { test: path.join(__dirname, 'es6'),
+                  loader: 'babel-loader?presets[]=es2015' },
+
+                { test: /\.scss$/,
+                    loader: ['style','css','postcss','sass'] }
+            ]
+        },
+        postcss: function(){
+            return [lost,autoprefixer({
+                browsers:['last 3 versions']
+            })];
+        }
+    };
